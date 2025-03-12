83020ff8d716e22c8a805127ec4bfa4105d96f22
saitejamoluguri <saitejamoluguri2@gmail.com>
2025-03-12 12:36:45 +0530

    added react app

---
+++ my-react-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Hello World!</h1>
+    <h1>This is my first React App</h1>
+  </div>
+  );
+}
+
+export default App;
